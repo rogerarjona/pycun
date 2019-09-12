bb369da1733a057eeaf2af667dab8c83ffcbae4f
Roger Arjona <server1.wisphub@gmail.com>
2019-09-12 17:47:32 -0500

    First Commit

---
+++ book/models.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=60)
+    estado = models.CharField(max_length=30)
+    pais = models.CharField(max_length=50)
+    website = models.URLField()
+
+    def __unicode__(self):
+        return self.nombre
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    def __unicode__(self):
+        return u'%s %s' % (self.nombre, self.apellido)
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autores = models.ManyToManyField(Autor)
+    editor = models.ForeignKey(Editorial)
+    fecha_publicacion = models.DateField()
+
+    def __unicode__(self):
+        return self.titulo
